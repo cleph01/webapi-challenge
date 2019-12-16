828897dfba909a0d64365ee758373fbbaff8faf2
Charlie <charles.j.montoya@gmail.com>
2019-12-15 21:56:18 -0500

    MVP done

--- index.js
+++ index.js
@@ -1,14 +1,57 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
 
-Sing along:
+const express = require('express');
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, just API
+//Instead of initiating dotenv config here,
+//we are adding a -r flag to the server initiator code
+//in the package.json file
 
-Go code!
-*/
+// const dotenv = require('dotenv')
+
+// dotenv.config()
+
+const helmet = require('helmet');
+
+// const logger = require('./middleware/logger');
+
+const actionRoutes = require('./actions/actionRouter');
+const projectRoutes = require('./projects/projectRouter');
+
+
+const server = express();
+
+server.use(helmet())
+
+// server.use(logger());
+
+
+//Using some middleware to parse request body if its JSON
+server.use(express.json());
+
+
+//Using imported userRoutes
+server.use('/actions', actionRoutes);
+server.use('/projects', projectRoutes);
+
+
+//Home Page
+server.use('/', (req, res) => {
+
+    const name = process.env.MY_NAME
+
+    res.send(`Welcome To ${name}'s Sprint Page`)
+  });
+
+
+
+
+ 
+
+
+const port = process.env.PORT || 5000; //yea
+
+const host = process.env.HOST || "0.0.0.0";
+
+// watch for connections on port 5000
+server.listen(port, host, () =>
+  console.log('Server running on http://localhost:5000')
+);
